c4622970e554874a4e2db0336ae06a1664afce53
SD-PARK <aa55235490@gmail.com>
2024-08-16 10:23:56 +0900

    refact: 불필요한 코드 제거

--- DriveWise/src/main/java/com/drivewise/smarttraffic/controller/RouteController.java
+++ DriveWise/src/main/java/com/drivewise/smarttraffic/controller/RouteController.java
@@ -9,8 +9,5 @@ import com.drivewise.smrattraffic.pridiction.TravelTimePrediction;
 public class RouteController {
 	
 	@GetMapping("/route")
-	public void getRoute() {
-		int time = new TravelTimePrediction().predict(10, 10.0, 1, 50, 120.0, 40.0);
-		System.out.println(time);
-	}
+	public void getRoute() {}
 }

8f63bb8eecb8af67025dc41efc416f2c5b4fd79a
SD-PARK <aa55235490@gmail.com>
2024-08-16 10:23:16 +0900

    fix: hour에 10 미만의 값이 들어가지 못하는 문제 수정

--- 02-Data/02-R/travel_time_prediction.R
+++ 02-Data/02-R/travel_time_prediction.R
@@ -3,8 +3,6 @@ setwd("C:/Users/bit/git/DriveWise/02-Data/02-R")
 load("travel_time_model.RData")
 
 predictTravelTime <- function(hour, average_speed, lanes, max_speed_limit, length, tci) {
-  hour <- sprintf("%02d", hour)
-  
   input_data <- data.frame(
     hour = as.factor(hour),
     average_speed = average_speed,
@@ -17,10 +15,3 @@ predictTravelTime <- function(hour, average_speed, lanes, max_speed_limit, lengt
   predicted_travel_time <- predict(model.2, newdata=input_data)
   return(predicted_travel_time)
 }
-
-testFnc <- function(hour, average_speed, lanes, max_speed_limit, length, tci) {
-  time <- predictTravelTime(10, 10.000000, 2, 60, 33.900000, 30.000000)
-  return(time)
-}
-
-testFnc(10, 10.000000, 2, 60, 33.900000, 30.000000)

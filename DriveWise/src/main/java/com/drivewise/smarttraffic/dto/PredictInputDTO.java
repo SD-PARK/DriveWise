83b6a1b7bfc5be7b98d3e9e21ad7af4852129f83
SD-PARK <aa55235490@gmail.com>
2024-08-21 13:49:12 +0900

    refact: 실시간 데이터 필드의 타입 변경(원시 -> 참조)

--- DriveWise/src/main/java/com/drivewise/smarttraffic/dto/PredictInputDTO.java
+++ DriveWise/src/main/java/com/drivewise/smarttraffic/dto/PredictInputDTO.java
@@ -4,7 +4,9 @@ import lombok.Data;
 
 @Data
 public class PredictInputDTO {
-	int hour, averageSpeed, lanes, maxSpeedLimit, visitorCount;
-	float length;
-	double tci;
+	// Null 여부를 판별해야 하는 데이터는 참조 타입, 그 외는 기본 타입 지정
+	int hour, averageSpeed, lanes, maxSpeedLimit;
+	double length;
+	Integer visitorCount;
+	Double tci;
 }

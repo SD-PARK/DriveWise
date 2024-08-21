9cf61de93dea259572fc192767061010a0ec468b
SD-PARK <aa55235490@gmail.com>
2024-08-21 13:40:48 +0900

    feat: 스케줄링 단위 수정 및 PredictionHandler 추가

--- DriveWise/src/main/java/com/drivewise/smarttraffic/scheduler/DataFetchingScheduler.java
+++ DriveWise/src/main/java/com/drivewise/smarttraffic/scheduler/DataFetchingScheduler.java
@@ -4,7 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.drivewise.smarttraffic.invoker.APIFetcherInvoker;
+import com.drivewise.smarttraffic.api.APIFetcherInvoker;
+import com.drivewise.smarttraffic.api.PredictionHandler;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,13 +13,15 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class DataFetchingScheduler {
 	@Autowired
-	APIFetcherInvoker apiFetcherInvoker;
-	
-	public static final long SCHEDULE_INTERVAL_MS = 300000;
-	
-	@Scheduled(fixedRate = SCHEDULE_INTERVAL_MS)
+	private APIFetcherInvoker apiFetcherInvoker;
+	@Autowired
+	private PredictionHandler predictionHandler;
+
+	@Scheduled(cron = "0 0/5 * * * ?")
 	public void runTasks() {
-		log.info("작업 실행 중");
+		log.info("작업 중");
 		apiFetcherInvoker.invoke();
+		predictionHandler.runPredict();
+		log.info("작업 완료");
 	}
 }

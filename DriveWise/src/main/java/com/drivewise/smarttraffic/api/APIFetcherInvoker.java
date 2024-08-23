277bfde93c1cf293eab5db80d5535a850bf3a92d
SD-PARK <aa55235490@gmail.com>
2024-08-23 09:30:58 +0900

    refact: 의존성 주입 코드 개선

--- DriveWise/src/main/java/com/drivewise/smarttraffic/api/APIFetcherInvoker.java
+++ DriveWise/src/main/java/com/drivewise/smarttraffic/api/APIFetcherInvoker.java
@@ -1,29 +1,38 @@
 package com.drivewise.smarttraffic.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.drivewise.smarttraffic.api.dto.LinkTrafficDTO;
 import com.drivewise.smarttraffic.api.fetcher.IAPIFetcher;
-import com.drivewise.smarttraffic.dto.LinkTrafficDTO;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
 public class APIFetcherInvoker {
-    private final IAPIFetcher<LinkTrafficDTO> linkTrafficFetcher;
+    private List<IAPIFetcher<?>> fetcherList;
     
     @Autowired
     public APIFetcherInvoker(
         IAPIFetcher<LinkTrafficDTO> linkTrafficFetcher
     ) {
-        this.linkTrafficFetcher = linkTrafficFetcher;
+    	this.fetcherList = new ArrayList<>();
+        this.fetcherList.add(linkTrafficFetcher);
     }
 	
 	public void invoke() {
-		LinkTrafficDTO linkTraffics = linkTrafficFetcher.fetch();
-		log.info("실시간 교통정보 수집 완료");
-		linkTrafficFetcher.save(linkTraffics);
-		log.info("실시간 교통정보 저장 완료");
+		for (IAPIFetcher<?> fetcher: fetcherList) {
+			invokeFetcher(fetcher);
+		}
+		log.info("실시간 데이터 저장 완료");
 	}
+
+    private <T> void invokeFetcher(IAPIFetcher<T> fetcher) {
+        T data = fetcher.fetch();
+        fetcher.save(data);
+    }
 }
